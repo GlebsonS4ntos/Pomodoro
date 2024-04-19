54bbf89c9473038c9ae941c98c72dce4080a14c9
Glebson Santos <glebsonsantos@outlook.pt>
2024-04-19 20:08:59 -0300

    Projeto terminado

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const buttonBreak = document.querySelector(".break");
 
 let totalSeconds = 0; 
 let timerInterval; 
-let play = true;
+let play = true; //verifica se e play com true, false é se for pause
 let pomodoroOrBreak = true; //caso seja true é um pomodoro caso seja false um break
 
 
@@ -17,19 +17,19 @@ function PlayPause(){
     if (play) { //caso seja play vai ser iniciado o timer 
         players[0].style.opacity = 0;
         players[1].style.opacity = 1;
-        pomodoroOrBreak ? GoTimer("pomodoro") : GoTimer("break");
-        play = !play;
+        pomodoroOrBreak ? GoTimer("pomodoro") : GoTimer("break"); 
     } else {
         players[0].style.opacity = 1;
         players[1].style.opacity = 0;
         GoTimer(true);
-    }    
+    }
+    play = !play;    
 } 
 
 function GoTimer(stopOrInit) {
     if (typeof stopOrInit === "string" && totalSeconds == 0) {
         //Se não for para parar e o tempo total é zero, inicializa o tempo total baseado no pomodoro ou break
-        totalSeconds = (stopOrInit === "pomodoro") ? pomodoroTime * 60 : breakTime * 60 - 1;
+        totalSeconds = (stopOrInit === "pomodoro") ? pomodoroTime * 60 : breakTime * 60;
     }
 
     clearInterval(timerInterval);
@@ -39,11 +39,12 @@ function GoTimer(stopOrInit) {
     function updateTimer() {
         //Verifica se o tempo acabou ou se a contagem deve ser parar
         if (totalSeconds <= 0) {
-            clearInterval(timerInterval);
+            const audio = document.querySelector("audio");
+            audio.play();
             PlayPause();
+            clearInterval(timerInterval);
         } else if (stopOrInit === true) {
             clearInterval(timerInterval);
-            play = !play;
         } else {
             totalSeconds--;
         }
@@ -58,27 +59,54 @@ function GoTimer(stopOrInit) {
 }
 
 buttonPomodoro.addEventListener('click', () => {
-    pomodoroOrBreak = true;
-    minutes.textContent = "25";
-    seconds.textContent = "00";
-    //reseta o contador
+    if(pomodoroOrBreak == false){ //so muda o valor e chama a animacao quandor um break for o ultimo chamado
+        pomodoroOrBreak = true;
+        AnimationRotate();
+    }
+    setTimeout(() => { //valor vai ser alterado no meio da animacao de rotacao
+        minutes.textContent = "25";
+        seconds.textContent = "00";
+    }, 500);
+    if (!play) { //caso tiver um temporizador em execucao mandar parar
+        PlayPause();
+    } 
     clearInterval(timerInterval);
     totalSeconds = 0;
-    if (!play) {
-        PlayPause();
-        play = !play;
-    } 
 });
 
 buttonBreak.addEventListener('click', () => {
-    pomodoroOrBreak = false;
-    minutes.textContent = "05";
-    seconds.textContent = "00";
-    //reseta o contador
-    clearInterval(timerInterval);
-    totalSeconds = 0;
+    if (pomodoroOrBreak == true){ //so muda o valor e chama a animacao quandor um pomodoro for o ultimo chamado
+        pomodoroOrBreak = false;
+        AnimationRotate();
+    }
+    setTimeout(() => { //valor vai ser alterado no meio da animacao de rotacao
+        minutes.textContent = "05";
+        seconds.textContent = "00";
+   }, 500);
     if (!play) {
         PlayPause();
-        play = !play;
     } 
+    clearInterval(timerInterval);
+    totalSeconds = 0;
 });
+
+function AnimationRotate(){
+    const circle = document.querySelector(".circle");
+    const stopwatch = document.querySelector(".stopwatch");
+
+    circle.classList.add("classeAnimationFlip");
+    setTimeout(() => {
+        circle.classList.remove("classeAnimationFlip");
+    }, 1000); //remove a classe dps de 1s que é o tempo de ocorrer a animacao
+    if(pomodoroOrBreak){
+        setTimeout(() => {
+            circle.style.boxShadow = "-1px 0px 10px 4px #33ba33";
+            stopwatch.style.border = "solid 5px #ce4320";    
+        }, 500);
+    }else{
+        setTimeout(() => {
+            circle.style.boxShadow = "-1px 0px 10px 4px #3586a3";
+            stopwatch.style.border = "solid 5px #0b1153";
+        }, 500);
+    }
+}
